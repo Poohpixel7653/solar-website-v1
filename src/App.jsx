cbecfd54897584dace423305137cfaca8759ea20
DESKTOP-UQLAOAH\PoohPixel <pu_k5@hotmail.com>
2024-03-12 15:56:00 +0700

    update loader when entry on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import {
   About,
@@ -11,24 +11,47 @@ import {
 } from "./components";
 import { RevealOnScroll } from "./utils/revealOnScroll";
 
+import PuffLoader from "react-spinners/PuffLoader";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <section>
-      <NavBar />
-      <Hero id="home" />
-      <RevealOnScroll>
-        <Service id="service" />
-      </RevealOnScroll>
-      <RevealOnScroll>
-        <CustomerMap id="map" />
-      </RevealOnScroll>
-      <RevealOnScroll>
-        <CustomerFeedback id="feedback" />
-      </RevealOnScroll>
-      <RevealOnScroll>
-        <Footer id="contact" />
-      </RevealOnScroll>
-    </section>
+    <>
+      {isLoading ? (
+        <div className="h-screen flex items-center justify-center">
+          <PuffLoader
+          loading={isLoading}
+          size={150}
+          color="#208486"
+        />
+        </div>
+      ) : (
+        <section>
+          <NavBar />
+          <Hero id="home" />
+          <RevealOnScroll>
+            <Service id="service" />
+          </RevealOnScroll>
+          <RevealOnScroll>
+            <CustomerMap id="map" />
+          </RevealOnScroll>
+          <RevealOnScroll>
+            <CustomerFeedback id="feedback" />
+          </RevealOnScroll>
+          <RevealOnScroll>
+            <Footer id="contact" />
+          </RevealOnScroll>
+        </section>
+      )}
+    </>
   );
 }
 
